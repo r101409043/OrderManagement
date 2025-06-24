bfa5a582cd8d1c67884801bf04bd2018fa6274d5
Yu-Lung,Chen <r101409043@gmail.com>
2025-06-24 12:28:25 -0400

    Initial push with infrastructure, services, and API

---
+++ Order.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Order.ApplicationCore.Contracts.Repositories;
+using Order.ApplicationCore.Contracts.Services;
+using Order.Infrastructure.Data;
+using Order.Infrastructure.Repositories;
+using Order.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add DbContext
+builder.Services.AddDbContext<OrderDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<OrderHistoryDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Register DI
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
